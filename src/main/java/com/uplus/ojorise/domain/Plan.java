d48f332f3516d2ae926216091240014dca76aa58
kuru2141 <kuru2141@naver.com>
2025-06-11 14:26:41 +0900

    OJORISE-54 add findPlanByProps

---
+++ src/main/java/com/uplus/ojorise/domain/Plan.java
@@ -0,0 +1,38 @@
+package com.uplus.ojorise.domain;
+
+import lombok.*;
+
+enum Eligibility {
+    ALL, 청소년, 청년, 시니어
+}
+
+enum DataType {
+    무제한, 제한
+}
+
+enum TelecomProvider {
+    SKT, LG, KT
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Plan {
+    private String name;
+    private int baseDataGb;
+    private int dailyDataGb;
+    private int dailyDataMb;
+    private int sharingDataGb;
+    private int sharingDataMb;
+    private int monthlyFee;
+    private int voiceCallPrice;
+    private boolean smsIncluded;
+    private int throttleSpeedKbps;
+    private Eligibility eligibility;
+    private String mobileType;
+    private DataType dataType;
+    private String planUrl;
+    private TelecomProvider telecomProvider;
+}
