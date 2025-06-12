15e862340e9cd55a65f38a17a486d233e6d0b716
qowldk <qowldk1030@gmail.com>
2025-06-12 17:43:51 +0900

    OJORISE-52 REFACTOR: 찜한 요금제 Domain 수정

--- src/main/java/com/uplus/ojorise/domain/DipPlan.java
+++ src/main/java/com/uplus/ojorise/domain/DipPlan.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class DipPlan {
-    private int id;
-    private int planId;
+    private int dipId;     // AUTO_INCREMENT PK
+    private int id;        // 사용자 ID (user.id)
+    private int planId;    // 요금제 ID (plan.plan_id)
 }
