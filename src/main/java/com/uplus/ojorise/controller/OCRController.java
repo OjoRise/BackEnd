2edd335adc233fda590548e817f31305fa558cd3
sujinRo <shtnwls1111@ewhain.net>
2025-06-13 19:13:23 +0900

    OJORISE-63 FEAT: google cloud vision api 이용하여 ocr 연결 및 POST API 구현

---
+++ src/main/java/com/uplus/ojorise/controller/OCRController.java
@@ -0,0 +1,33 @@
+package com.uplus.ojorise.controller;
+
+import com.uplus.ojorise.service.GoogleApiService;
+import com.uplus.ojorise.service.KakaoApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class OCRController {
+
+    private final GoogleApiService googleApiService;
+
+    @PostMapping(value = "/google/ocr", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Google OCR 실행", description = "이미지 업로드 시, Google OCR 처리 후 결과 반환")
+    public ResponseEntity<String> runOCR(@RequestParam("image")MultipartFile file) {
+        try {
+            String result = googleApiService.googleOCR(file);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("OCR 실패: " + e.getMessage());
+        }
+    }
+}
