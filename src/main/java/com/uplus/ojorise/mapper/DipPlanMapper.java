19da66f76f2a87899e6a6cf8588733bb9ba18ab8
sujinRo <shtnwls1111@ewhain.net>
2025-06-23 00:22:30 +0900

    OJORISE-87 FEAT: RecommendPlan과 DipPlan xml로 mapper 나누기

--- src/main/java/com/uplus/ojorise/mapper/DipPlanMapper.java
+++ src/main/java/com/uplus/ojorise/mapper/DipPlanMapper.java
@@ -9,12 +9,6 @@ import java.util.List;
 @Mapper
 public interface DipPlanMapper {
 
-    @Select("""
-        SELECT p.*
-        FROM dipplan d
-        JOIN plan p ON d.plan_id = p.plan_id
-        WHERE d.id = #{id}
-    """)
     List<Plan> findByUserId(int id);
 
     @Delete("DELETE FROM dipplan WHERE id = #{id} AND plan_id = #{planId}")
